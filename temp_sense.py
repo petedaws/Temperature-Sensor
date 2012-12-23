62996454388d0d0c8fcda2ed5777751152bc0bc1
Peter Dawson <petedaws@gmail.com>
2012-12-23 22:10:53 +1000

    basic logging of the 3 analog temp values

--- temp_sense.py
+++ temp_sense.py
@@ -1,9 +1,26 @@
 #! /usr/bin/python
+# RUN COMMAND:  nohup python "/home/pi/dev/temp_sense.py" start &
 
 import serial
+import time
+import sys
+import string
+
+def main(logname):
+	serialport = serial.Serial("/dev/ttyUSB0", 9600)
+	while(True):
+		temps = serialport.readline(None).strip()
+		if not all(c in string.printable for c in temps):
+			continue
+		log = open(logname,'a')
+		tm = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+		log.write("".join((tm,',',temps,'\n')))
+		log.close()
+
+if __name__ == "__main__":
+	if sys.argv[1] == 'start':
+		main("/home/pi/temp.log")
+	else:
+		sys.exit(0)
 
 
-serialport = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
-while(True):
-        response = serialport.readlines(None)
-        print response
