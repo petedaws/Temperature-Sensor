7b5fb4d0c9b04c547bad065ffc86205991d062e5
Peter Dawson <petedaws@gmail.com>
2012-12-28 00:25:28 +1000

    averaging added

--- temp_read/temp_read.pde
+++ temp_read/temp_read.pde
@@ -4,21 +4,21 @@ float temp_calibrate[4] = {0.0 , 0.0 , 0.0 , 0.0};
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("Starting Temperature Monitoring"); 
 }
 
 void print_temps()
 {
-  for(int i=0;i++;i<4){
+  for(int i=0;i<3;i++){
     Serial.print(temp[i]);
     Serial.print(",");
   }
+  Serial.print(temp[4]);
   Serial.println();
 }
 
 void loop()
 {
-  for(int i=0;i++;i<4){
+  for(int i=0;i<4;i++){
     temp[i] = analogRead(i) * 0.004882812 * 100 - 273.15 + temp_calibrate[i];
   }  
   print_temps();
--- temp_sense.py
+++ temp_sense.py
@@ -5,23 +5,53 @@ import serial
 import time
 import sys
 import string
+import platform
 
-def main(logname):
-	serialport = serial.Serial("/dev/ttyUSB0", 9600)
+calibrations = {0:0.0, 1:0.0, 2: 0.0}
+
+def average(data):
+	readings = zip(*data)
+	average_result = []
+	for reading in readings:
+		total = 0
+		for sensor in reading:
+			total += sensor
+		average_result.append(total/float(len(reading)))
+	return average_result
+	
+def calibrate(temp):
+	for k,v in calibrations.iteritems():
+		temp[k] += v
+	return temp
+
+def main(logname,ave_window):
+	if platform.system() == 'Windows':
+		serialport = serial.Serial("COM7", 9600)
+	else:
+		serialport = serial.Serial("/dev/ttyUSB0", 9600)
+	temps = []
 	while(True):
 		tempstr = serialport.readline(None).strip()
-		if not all(c in string.printable for c in temps):
+		if not all(c in string.printable for c in tempstr):
 			continue
-		temps = [float(i) for i in tempstr.split(',')]
-		temps_calibrate = 
-		log = open(logname,'a')
-		tm = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
-		log.write("".join((tm,',',temps,'\n')))
-		log.close()
+		temp = [float(i) for i in tempstr.split(',')]
+		temps.append(calibrate(temp))
+
+		if len(temps) > ave_window:
+			average_result = average(temps)
+			log = open(logname,'a')
+			tm = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+			log.write("".join((tm,',',",".join("%0.1f" % x for x in average_result),'\n')))
+			print "average " + "".join((tm,',',",".join("%0.1f" % x for x in average_result),'\n'))
+			log.close()
+			temps = []
 
 if __name__ == "__main__":
 	if sys.argv[1] == 'start':
-		main("/home/pi/temp.csv")
+		if platform.system() == 'Windows':
+			main("temp.csv",30)
+		else:
+			main("/home/pi/temp.csv",30)
 	else:
 		sys.exit(0)
 
