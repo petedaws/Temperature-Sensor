3c5e0f9df8c2fb192d9ffad9e2ff1b2ad87e9db6
Peter Dawson <petedaws@gmail.com>
2012-12-31 10:50:14 +1000

    data now stored in a single json file

--- web-dev/pure_js/process.py
+++ web-dev/pure_js/process.py
@@ -17,8 +17,6 @@ for row in csvrdr:
 	s1.append([(long(s)*1000),float(row[2])])
 	s2.append([(long(s)*1000),float(row[3])])
 	s3.append([(long(s)*1000),float(row[4])])
+	data = {'s0':s0,'s1':s1,'s2':s2}
 	
-open('s0.json','wb').write(json.dumps(s0,indent=4))
-open('s1.json','wb').write(json.dumps(s1,indent=4))
-open('s2.json','wb').write(json.dumps(s2,indent=4))
-open('s3.json','wb').write(json.dumps(s3,indent=4))
+open('temp_data.json','wb').write(json.dumps(data,indent=4))
