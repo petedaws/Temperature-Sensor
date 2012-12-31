b7757a3a9d6fffa4958490fcb50beffd62d2dd6c
Peter Dawson <petedaws@gmail.com>
2012-12-31 13:15:47 +1000

    installed the temperature monitor, refresh only. Running successfully on the netbook.

--- temp_sense.py
+++ temp_sense.py
@@ -6,8 +6,31 @@ import time
 import sys
 import string
 import platform
+import csv
+import datetime
+import json
 
 calibrations = {0:0.0, 1:0.0, 2: 0.0}
+
+def output_json(datafile):
+	csvrdr  = csv.reader(open(datafile,'rb'))
+
+	s0 = []
+	s1 = []
+	s2 = []
+	s3 = []
+
+	for row in csvrdr:
+
+		s = time.mktime(datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S').timetuple())
+		s0.append([(long(s)*1000),float(row[1])])
+		s1.append([(long(s)*1000),float(row[2])])
+		s2.append([(long(s)*1000),float(row[3])])
+		s3.append([(long(s)*1000),float(row[4])])
+		data = {'s0':s0,'s1':s1,'s2':s2}
+	output_file = open('/var/www/temp_data.json','wb')
+	output_file.write(json.dumps(data,indent=4))
+	output_file.close()
 
 def average(data):
 	readings = zip(*data)
@@ -47,6 +70,7 @@ def main(logname,ave_window):
 			log.write("".join((tm,',',",".join("%0.1f" % x for x in average_result),'\n')))
 			log.close()
 			temps = []
+			output_json(logname)
 
 if __name__ == "__main__":
 	if sys.argv[1] == 'start':
--- web-dev/pure_js/process.py
+++ web-dev/pure_js/process.py
@@ -3,20 +3,24 @@ import time
 import datetime
 import json
 
-csvrdr  = csv.reader(open('temp.csv','rb'))
+def output_json(datafile):
+	csvrdr  = csv.reader(open(datafile,'rb'))
 
-s0 = []
-s1 = []
-s2 = []
-s3 = []
+	s0 = []
+	s1 = []
+	s2 = []
+	s3 = []
 
-for row in csvrdr:
+	for row in csvrdr:
 
-	s = time.mktime(datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S').timetuple())
-	s0.append([(long(s)*1000),float(row[1])])
-	s1.append([(long(s)*1000),float(row[2])])
-	s2.append([(long(s)*1000),float(row[3])])
-	s3.append([(long(s)*1000),float(row[4])])
-	data = {'s0':s0,'s1':s1,'s2':s2}
-	
-open('temp_data.json','wb').write(json.dumps(data,indent=4))
+		s = time.mktime(datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S').timetuple())
+		s0.append([(long(s)*1000),float(row[1])])
+		s1.append([(long(s)*1000),float(row[2])])
+		s2.append([(long(s)*1000),float(row[3])])
+		s3.append([(long(s)*1000),float(row[4])])
+		data = {'s0':s0,'s1':s1,'s2':s2}
+		
+	open('temp_data.json','wb').write(json.dumps(data,indent=4))
+
+if __name__ == "__main__":
+	output_json('temp.csv')
